da791cb9159cc924f742861ed01e26260b645ea6
Franco Uriel Arto Vazquez <franco.arto@globant.com>
2019-01-29 17:03:15 -0300

    Passing default tests fix

--- App/src/app/app.component.spec.ts
+++ App/src/app/app.component.spec.ts
@@ -32,7 +32,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Crypto Market'`, () => {
+  it(`should have as title 'CryptoMarket'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('CryptoMarket');
